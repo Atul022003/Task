cb940ae309bb3f654d5a29c0309eb6651cfdfc67
7081295555 <sontysinghmmw485@gmail.com>
2024-12-12 22:14:57 +0530

    - Made some changes in delete and logout.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task/add_product.dart';
 import 'package:task/login_page.dart';
+import 'package:task/modal/Product.dart';
 import 'package:task/provider/auth_provider.dart';
 import 'package:task/provider/product_provider.dart';
 
@@ -39,8 +40,41 @@ class HomePageState extends State<HomePage>{
     appBar: AppBar(
       actions: [
         IconButton(onPressed: (){
-          authProvider.logout();
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> LoginPage()));
+
+          showDialog<void>(
+            context: context,
+            barrierDismissible: false, // user must tap button!
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Logout?'),
+                content:  SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                      Text('Are you sure you want to logout?'),
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Logout'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      authProvider.logout();
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> LoginPage()));
+
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+
 
         }, icon: Icon(Icons.logout))
 
@@ -70,15 +104,14 @@ class HomePageState extends State<HomePage>{
           height: 10,
         ),
 
-        (provider.allProducts.isNotEmpty) ? Column(children: provider.allProducts.map((product) =>
-        ListTile(
-          leading: (product.imagePath.isNotEmpty) ? Image.file(File(product.imagePath),
-            fit: BoxFit.cover,width: 70,height: 70,) : SizedBox(),
-          title: Text(product.name ?? "~"),
-          subtitle: Text("${product.price}"),trailing: InkWell(child: Icon(Icons.delete),
-        onTap: (){
-            provider.deleteProduct(product);
-        },),)).toList(),) : Column(children: [
+        (provider.allProducts.isNotEmpty) ? GridView.count(
+          mainAxisSpacing: 10,
+    crossAxisSpacing: 10,
+    primary: false,
+    padding:const EdgeInsets.all(10),
+    crossAxisCount:  2,
+    shrinkWrap: true,
+    children: provider.allProducts.map((product) => ProductWidget(product,provider)).toList()) : Column(children: [
 
           Icon(Icons.list),
           Text("No Products Found")
@@ -92,6 +125,73 @@ class HomePageState extends State<HomePage>{
 
 
   );
+  }
+
+  Widget ProductWidget(Product product,ProductProvider provider){
+    return Stack(children: [
+
+      Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.grey.shade200,
+        ),
+        child: Column(children: [
+          SizedBox(height: 20,),
+          (product.imagePath.isNotEmpty) ? Image.file(File(product.imagePath),height: 100,) : SizedBox(),
+          SizedBox(height: 10,),
+          Text(product.name,style: TextStyle(fontSize:18,fontWeight: FontWeight.bold),),
+          Text("₹ ${product.price}"),
+
+        ],),),
+
+      Align(alignment: Alignment.topRight,
+      child: InkWell(onTap: (){
+
+        showDialog<void>(
+          context: context,
+          barrierDismissible: false, // user must tap button!
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Delete Product?'),
+              content:  SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Are you sure you want to delete "${product.name}" from product list?'),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: const Text('Delete'),
+                  onPressed: () {
+                    provider.deleteProduct(product);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+
+
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.red.shade100,
+        ),
+        padding: EdgeInsets.all(10),
+        child:Icon(Icons.delete_outline_rounded,color: Colors.red,) ,),),)
+
+
+    ],);
   }
 
 }
